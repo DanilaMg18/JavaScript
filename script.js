4f4dff27153ae5568b133ab5bd0b986084648c8d
Danila Palcech <dpalcech@gmail.com>
2022-09-19 13:48:01 +0300

    javacript

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function sum() {
+let a = 11
+let b = 23
+return a + b
+}
+
+let result = sum()
+
+console.log(result)
+
+function multiply() {
+    let a = 4
+    let b = 2
+    return a * b
+
+}
+
+let result2 = multiply()
+
+console.log(result2)
